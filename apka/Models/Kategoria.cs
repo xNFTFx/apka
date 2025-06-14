a70b4c35e7aa56110440741ece7aa6fa3b350f14
xNFTFx <szbuchowicz@gmail.com>
2025-06-14 23:03:50 +0200

    proba

---
+++ apka/Models/Kategoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace apka.Models
+{
+    public class Kategoria
+    {
+        [Key]
+        public int IdKategoria { get; set; }
+        [Required]
+        public string Nazwa { get; set; }
+        
+        public Kategoria(string nazwa)
+        {
+            Nazwa = nazwa;
+        }
+        
+        public Kategoria() { } // Default constructor for EF Core
+    }
+    
