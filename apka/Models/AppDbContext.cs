e3f0ec60b1e3634079d4baef212cbae1a93995d5
xNFTFx <szbuchowicz@gmail.com>
2025-06-14 23:19:55 +0200

    context

---
+++ apka/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore; 
+using System.ComponentModel.DataAnnotations;
+
+namespace apka.Models
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produkt> Produkty { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Poziom> Poziomy { get; set; }
+        public DbSet<Autor> Autorzy { get; set; }
+    }
+}
