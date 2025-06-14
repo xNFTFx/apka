6216ccf4e4727b5844db99ccad2165bf24607249
xNFTFx <szbuchowicz@gmail.com>
2025-06-14 23:59:39 +0200

    contr

---
+++ apka/Controllers/KategoriasController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using apka.Models;
+
+namespace apka.Controllers
+{
+    public class KategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public KategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Kategorias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Kategorie.ToListAsync());
+        }
+
+        // GET: Kategorias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategoria = await _context.Kategorie
+                .FirstOrDefaultAsync(m => m.IdKategoria == id);
+            if (kategoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(kategoria);
+        }
+
+        // GET: Kategorias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kategorias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdKategoria,Nazwa")] Kategoria kategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kategoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategoria);
+        }
+
+        // GET: Kategorias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategoria = await _context.Kategorie.FindAsync(id);
+            if (kategoria == null)
+            {
+                return NotFound();
+            }
+            return View(kategoria);
+        }
+
+        // POST: Kategorias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdKategoria,Nazwa")] Kategoria kategoria)
+        {
+            if (id != kategoria.IdKategoria)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kategoria);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KategoriaExists(kategoria.IdKategoria))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategoria);
+        }
+
+        // GET: Kategorias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategoria = await _context.Kategorie
+                .FirstOrDefaultAsync(m => m.IdKategoria == id);
+            if (kategoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(kategoria);
+        }
+
+        // POST: Kategorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var kategoria = await _context.Kategorie.FindAsync(id);
+            if (kategoria != null)
+            {
+                _context.Kategorie.Remove(kategoria);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KategoriaExists(int id)
+        {
+            return _context.Kategorie.Any(e => e.IdKategoria == id);
+        }
+    }
+}
---
+++ apka/Controllers/PoziomsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using apka.Models;
+
+namespace apka.Controllers
+{
+    public class PoziomsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PoziomsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pozioms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Poziomy.ToListAsync());
+        }
+
+        // GET: Pozioms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var poziom = await _context.Poziomy
+                .FirstOrDefaultAsync(m => m.IdPoziom == id);
+            if (poziom == null)
+            {
+                return NotFound();
+            }
+
+            return View(poziom);
+        }
+
+        // GET: Pozioms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pozioms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPoziom,Nazwa")] Poziom poziom)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(poziom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(poziom);
+        }
+
+        // GET: Pozioms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var poziom = await _context.Poziomy.FindAsync(id);
+            if (poziom == null)
+            {
+                return NotFound();
+            }
+            return View(poziom);
+        }
+
+        // POST: Pozioms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPoziom,Nazwa")] Poziom poziom)
+        {
+            if (id != poziom.IdPoziom)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(poziom);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PoziomExists(poziom.IdPoziom))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(poziom);
+        }
+
+        // GET: Pozioms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var poziom = await _context.Poziomy
+                .FirstOrDefaultAsync(m => m.IdPoziom == id);
+            if (poziom == null)
+            {
+                return NotFound();
+            }
+
+            return View(poziom);
+        }
+
+        // POST: Pozioms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var poziom = await _context.Poziomy.FindAsync(id);
+            if (poziom != null)
+            {
+                _context.Poziomy.Remove(poziom);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PoziomExists(int id)
+        {
+            return _context.Poziomy.Any(e => e.IdPoziom == id);
+        }
+    }
+}
