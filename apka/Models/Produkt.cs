013c329fcb48d2455a955a440878a880bcc3a319
xNFTFx <szbuchowicz@gmail.com>
2025-06-14 23:08:32 +0200

    fk ustawienie

--- apka/Models/Produkt.cs
+++ apka/Models/Produkt.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace apka.Models
 {
@@ -10,7 +11,15 @@ namespace apka.Models
         public string Nazwa { get; set; }
         [Required]
         public string Opis { get; set; }
-        
+        [ForeignKey("Kategoria")]
+        public int IdKategoria { get; set; }
+        [ForeignKey("Poziom")]
+        public int IdPoziom { get; set; }
+        [ForeignKey("Autor")]
+        public int IdAutor { get; set; }
+        [ForeignKey("Skladnik")]
+        public int IdSkladnik { get; set; }
+
         public Produkt(string nazwa, string opis)
         {
             Nazwa = nazwa;
